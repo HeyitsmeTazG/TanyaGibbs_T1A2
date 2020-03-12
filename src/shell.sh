7576d0a9ce630bb18a48ec4e6c74457834d9e41d
HeyitsmeTazG <tanyagibbs@outlook.com.au>
2020-03-12 15:48:47 +1000

    Added shell script

---
+++ src/shell.sh
@@ -0,0 +1,8 @@
+echo "Welcome to Computers Against Developers!"
+echo "Lets begin the install now"
+git clone "https://github.com/HeyitsmeTazG/compAgainstDevs_terminalApp.git"
+cd AttendanceLogger/src
+echo "Time to ensure all required gems are installed"
+bundle install
+echo "All gems have been installed, running application for the first time now - in future just type compAgainstDevs to open application"
+ruby compAgainstDevs.rb
